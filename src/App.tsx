b76ed056d6b4d75fedf40c0212ade19f16682437
Valiantsin Rakush <vrakush.external@epo.org>
2022-08-07 16:32:05 +0300

    no google auth

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,6 @@ import { deletePlayerFromGame } from './service/players';
 
 function App() {
 
-  // const user = useContext(AuthContext);
-  // // const provider = new GoogleAuthProvider();
-  // var provider = new firebase.auth.GoogleAuthProvider();
-  // firebase.auth()
-  // .signInWithPopup(provider)
-  // .then((result) => {
-  //   /** @type {firebase.auth.OAuthCredential} */
-  //   var credential = result.credential;
-
-  //   // This gives you a Google Access Token. You can use it to access the Google API.
-  //   // var token = credential?.accessToken;
-  //   // The signed-in user info.
-  //   var user = result.user;
-  //   console.log(`SUCCESS ${JSON.stringify(user)}`)
-  //   // ...
-  // }).catch((error) => {
-  //   console.log(`ERROR ${error.message}`)
-  //   // Handle Errors here.
-  //   var errorCode = error.code;
-  //   var errorMessage = error.message;
-  //   // The email of the user's account used.
-  //   var email = error.email;
-  //   // The firebase.auth.AuthCredential type that was used.
-  //   var credential = error.credential;
-  //   // ...
-  // });
-
   const [playerName, setPlayerName] = useState('');
   const [joinGameId, setJoinGameId] = useState('');
   
